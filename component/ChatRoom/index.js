eb0a43f31b8fd618de8302b3feca3f90a64adf7f
mirrorcall96 <mirrorcall96@gmail.com>
2021-08-02 17:28:33 +0300

    added menu / seen

--- component/ChatRoom/index.js
+++ component/ChatRoom/index.js
@@ -1,7 +1,7 @@
-import React, { PureComponent, useState } from "react";
-import { ScrollView, StyleSheet, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Image, Clipboard } from "react-native";
 import {
-  PermissionsAndroid,
+  TouchableOpacity,
   Text,
   View,
   TextField,
@@ -10,15 +10,30 @@ import {
   Constants,
   Spacings,
 } from "react-native-ui-lib";
-import { useSelector } from "react-redux";
-import { WebView } from "react-native-webview";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger,
+  MenuProvider,
+} from "react-native-popup-menu";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { deleteMessage } from "../../store/actions/chatActions";
 
 const ChatRoom = ({ route, socket }) => {
+  const dispatch = useDispatch();
   const user = useSelector((state) => state.user.user);
   let chats = useSelector((state) => state.chats.chats);
+
   let room = chats.find((chat) => route.params.room._id === chat._id);
   const [input, setInput] = useState("");
   const { messages, _id } = room;
+  useEffect(() => {
+    if (socket) {
+      socket.emit("roomSeen", { userId: user._id, roomId: _id });
+    }
+  }, []);
   const handleSubmit = () => {
     if (input === "") return;
     let content = {};
@@ -36,6 +51,36 @@ const ChatRoom = ({ route, socket }) => {
       <View flex>
         {messages.map((message) => {
           const isLeftBubble = user._id !== message.user._id;
+          let seenBy = message.receivers.filter(
+            (receiver) => receiver.seen !== null
+          );
+
+          // Check who recived the message from receivers
+          let receivedBy = message.receivers.filter(
+            (receiver) => receiver.received !== null
+          );
+          let seenStatus = <></>;
+          if (!isLeftBubble) {
+            if (seenBy.length === message.receivers.length) {
+              seenStatus = (
+                <Text dark10 text70 style={{ alignSelf: "flex-end" }}>
+                  <Icon name="check-circle" size={15} color={"#00FF00"} />
+                </Text>
+              );
+            } else if (receivedBy.length !== 0) {
+              seenStatus = (
+                <Text dark10 text70 style={{ alignSelf: "flex-end" }}>
+                  <Icon name="check-circle" size={15} color={"#ffffff"} />
+                </Text>
+              );
+            } else {
+              seenStatus = (
+                <Text dark10 text70 style={{ alignSelf: "flex-end" }}>
+                  <Icon name="check-circle-o" size={15} color={"#ffffff"} />
+                </Text>
+              );
+            }
+          }
           let type = message.content.type;
           let text;
 
@@ -88,7 +133,48 @@ const ChatRoom = ({ route, socket }) => {
                 padding-s2
                 width={"70%"}
               >
-                {text}
+                <TouchableOpacity onLongPress={() => console("test")}>
+                  <Menu>
+                    <MenuTrigger>
+                      <Text dark10 text70 style={{ alignSelf: "flex-end" }}>
+                        <Icon
+                          name="angle-down"
+                          size={20}
+                          color={!isLeftBubble ? "#ffffff" : "#333"}
+                        />
+                      </Text>
+                    </MenuTrigger>
+                    <MenuOptions>
+                      <MenuOption
+                        onSelect={() =>
+                          Clipboard.setString(message.content.text)
+                        }
+                        text="Copy"
+                      />
+                      <MenuOption
+                        onSelect={() =>
+                          dispatch(deleteMessage(user._id, message._id, _id))
+                        }
+                      >
+                        <Text style={{ color: "red" }}>Delete</Text>
+                      </MenuOption>
+                      {!isLeftBubble && (
+                        <MenuOption
+                          onSelect={() =>
+                            socket.emit("messageUpdate", {
+                              messageId: message._id,
+                              content: { text: "[deleted]", type: "deleted" },
+                            })
+                          }
+                        >
+                          <Text style={{ color: "red" }}>Delete for all</Text>
+                        </MenuOption>
+                      )}
+                    </MenuOptions>
+                  </Menu>
+                  {text}
+                  {seenStatus}
+                </TouchableOpacity>
               </View>
             </View>
           );
@@ -97,30 +183,39 @@ const ChatRoom = ({ route, socket }) => {
     );
   };
   return (
-    <View flex bg-dark80 paddingT-page>
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        {renderChatBubbles()}
-      </ScrollView>
-      <View bg-white row spread centerV paddingH-s5 paddingV-s3>
-        <TextField
-          containerStyle={{
-            flex: 1,
-            backgroundColor: Colors.grey60,
-            paddingVertical: Spacings.s2,
-            paddingHorizontal: Spacings.s4,
-            borderRadius: 8,
+    <MenuProvider>
+      <View flex bg-dark80 paddingT-page>
+        <ScrollView
+          contentContainerStyle={styles.scrollContainer}
+          ref={(ref) => {
+            scrollView = ref;
           }}
-          value={input}
-          onChangeText={setInput}
-          hideUnderline
-          placeholder={"Message"}
-          floatingPlaceholder={false}
-          floatOnFocus
-          enableErrors={false}
-        />
-        <Button onPress={handleSubmit} label="Send" link marginL-s4 />
+          onContentSizeChange={() => scrollView.scrollToEnd({ animated: true })}
+        >
+          {renderChatBubbles()}
+        </ScrollView>
+        <View bg-white row spread centerV paddingH-s5 paddingV-s3>
+          <TextField
+            containerStyle={{
+              flex: 1,
+              backgroundColor: Colors.grey60,
+              paddingVertical: Spacings.s2,
+              paddingHorizontal: Spacings.s4,
+              borderRadius: 8,
+            }}
+            value={input}
+            onChangeText={setInput}
+            hideUnderline
+            placeholder={"Message"}
+            floatingPlaceholder={false}
+            floatOnFocus
+            enableErrors={false}
+            onEndEditing={handleSubmit}
+          />
+          <Button onPress={handleSubmit} label="Send" link marginL-s4 />
+        </View>
       </View>
-    </View>
+    </MenuProvider>
   );
 };
 
